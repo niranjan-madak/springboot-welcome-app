bb0718cf71ba57f39d718339dd7c12348f10a2a4
niranjan.madak <nmadak28@gmail.com>
2022-12-14 10:35:18 +0530

    ok

---
+++ src/main/java/com/madak/SpringbootWelcomeAppApplication.java
@@ -0,0 +1,13 @@
+package com.madak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWelcomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWelcomeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madak/SpringbootWelcomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWelcomeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
