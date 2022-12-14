6a4da072eda77cbf2f972b1c847e3d1a6223b4e2
niranjan.madak <nmadak28@gmail.com>
2022-12-14 10:39:16 +0530

    second commit

--- src/main/java/com/madak/SpringbootWelcomeAppApplication.java
+++ src/main/java/com/madak/SpringbootWelcomeAppApplication.java
@@ -2,12 +2,20 @@ package com.madak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootWelcomeAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWelcomeAppApplication.class, args);
-	}
+    @GetMapping("/greet")
+    public String wlcmMsg() {
+        return "Hi niranjan welcome to your first spring boot app deployment on azure web app from github action !!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWelcomeAppApplication.class, args);
+    }
 
 }
